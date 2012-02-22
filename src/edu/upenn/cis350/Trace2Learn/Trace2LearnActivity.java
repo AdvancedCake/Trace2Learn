e630b4110f8d72cf3360d079d64cf6270ba8c368
Sam Appelbaum <lnk781@gmail.com>
2012-02-22 12:58:29 -0500

    initial commit

---
+++ src/edu/upenn/cis350/Trace2Learn/Trace2LearnActivity.java
@@ -0,0 +1,13 @@
+package edu.upenn.cis350.Trace2Learn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Trace2LearnActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
