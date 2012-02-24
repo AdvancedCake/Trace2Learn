ff4c9580b539d5445979b5336c60a6715ba848fc
Ryan Gormley <rgormley@seas.upenn.edu>
2012-02-24 01:44:54 -0500

    Initial commit of framework for drawing strokes. CharacterCreationActivity is the activity for designing characters. DrawingPane is the base class for the Views that will be used for creating and displaying characters. DrawingPane should be extended for its specific uses.

---
+++ src/edu/upenn/cis350/Trace2Learn/DrawingPane.java
@@ -0,0 +1,100 @@
+package edu.upenn.cis350.Trace2Learn;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingPane extends View {
+
+	private static final float MINP = 0.25f;
+	private static final float MAXP = 0.75f;
+
+	private Path mPath;
+	private Paint mPaint;
+	
+	private int mBackgroundColor = Color.GRAY;
+
+	public DrawingPane(Context c, Paint paint) {
+		super(c);
+
+		mPaint = paint;
+		mPath = new Path();
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawColor(mBackgroundColor);
+		canvas.drawPath(mPath, mPaint);
+	}
+
+	private float mX, mY;
+	private static final float TOUCH_TOLERANCE = 4;
+
+	private void touch_start(float x, float y) {
+		mPath.reset();
+		mPath.moveTo(x, y);
+		mX = x;
+		mY = y;
+		beginStroke();
+	}
+
+	private void touch_move(float x, float y) {
+		float dx = Math.abs(x - mX);
+		float dy = Math.abs(y - mY);
+		if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+			mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+			mX = x;
+			mY = y;
+		}
+	}
+
+	private void touch_up() {
+		mPath.lineTo(mX, mY);
+		completeStroke();
+	}
+	
+	private void beginStroke()
+	{
+		
+	}
+	
+	private void completeStroke()
+	{
+		// Override this for extended classes.
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		float x = event.getX();
+		float y = event.getY();
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			touch_start(x, y);
+			invalidate();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			touch_move(x, y);
+			invalidate();
+			break;
+		case MotionEvent.ACTION_UP:
+			touch_up();
+			invalidate();
+			break;
+		}
+		return true;
+	}
+}
