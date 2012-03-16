8a43ec7ca0fcf9632ce13f1bccb446583590f049
Bryan Chiang <bry.chiang@gmail.com>
2012-03-16 03:28:19 -0400

    create word based off of id of characters. changed LessonWord to be an array of Integers.

--- src/edu/upenn/cis350/Trace2Learn/Characters/LessonWord.java
+++ src/edu/upenn/cis350/Trace2Learn/Characters/LessonWord.java
@@ -9,31 +9,31 @@ import edu.upenn.cis350.Trace2Learn.Characters.LessonItem.ItemType;
 
 public class LessonWord extends LessonItem {
 	
-	private List<LessonCharacter> _characters;
+	private List<Integer> _characters;
 	
 	public LessonWord(){
 		_type = ItemType.WORD;
-		_characters = new ArrayList<LessonCharacter>();
+		_characters = new ArrayList<Integer>();
 	}
 	
-	public synchronized void addCharacter(LessonCharacter character){
+	public synchronized void addCharacter(Integer character){
 		_characters.add(character);
 	}
 	
-	public synchronized List<LessonCharacter> getCharacters(){
-		return new ArrayList<LessonCharacter>(_characters);
+	public synchronized List<Integer> getCharacters(){
+		return new ArrayList<Integer>(_characters);
 	}
 	
-	public synchronized LessonCharacter getCharacter(int i){
-		return _characters.get(i);
+	public synchronized int getCharacter(int i){
+		return _characters.get(i).intValue();
 	}
 	
-	public synchronized boolean removeCharacter(LessonCharacter character){
+	public synchronized boolean removeCharacter(Integer character){
 		return _characters.remove(character);
 	}
 	
-	public synchronized LessonCharacter removeCharacter(int i){
-		return _characters.remove(i);
+	public synchronized int removeCharacter(int i){
+		return _characters.remove(i).intValue();
 	}
 	
 	public synchronized void clearCharacters(){
