ca8d851d2fc4e48afc3b986c4e6aef30e02a6ba0
Sam Appelbaum <lnk781@gmail.com>
2012-03-16 08:00:54 -0400

    tests for non-UI classes

---
+++ tests/src/edu/upenn/cis350/Trace2Learn/test/DbAdapterTest.java
@@ -0,0 +1,11 @@
+package edu.upenn.cis350.Trace2Learn.test;
+
+import junit.framework.TestCase;
+
+public class DbAdapterTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
---
+++ tests/src/edu/upenn/cis350/Trace2Learn/test/LessonWordTest.java
@@ -0,0 +1,7 @@
+package edu.upenn.cis350.Trace2Learn.test;
+
+import junit.framework.TestCase;
+
+public class LessonWordTest extends TestCase {
+
+}
