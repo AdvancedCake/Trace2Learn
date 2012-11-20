00b17bae295ad39a862fe6e76f32dfacc5f86638
Robert Li <li.robertj+git@gmail.com>
2012-11-19 22:44:27 -0500

    merged changes to dbadapter, added tests for swapping chars and words

--- tests/src/edu/upenn/cis573/Trace2Win/test/DbAdapterTest.java
+++ tests/src/edu/upenn/cis573/Trace2Win/test/DbAdapterTest.java
@@ -41,6 +41,48 @@ public class DbAdapterTest extends AndroidTestCase {
 		w.addKeyValue("key1", "value1");
 		LessonWord w1 = db.getWordById(w.getId());
 		LessonWordTest.compareWords(w, w1);
-	}		
+	}
+
+    public void testSwapCharacters() {
+        LessonCharacter a = new LessonCharacter();
+        LessonCharacter b = new LessonCharacter();
+        db.addCharacter(a);
+        db.addCharacter(b);
+        double aOrigSort = a.getSort();
+        double bOrigSort = b.getSort();
+        assertFalse(aOrigSort == bOrigSort);
+        
+        assertTrue(db.swapCharacters(a.getId(), aOrigSort,
+                                     b.getId(), bOrigSort));
+        
+        LessonCharacter a1 = db.getCharacterById(a.getId());
+        LessonCharacter b1 = db.getCharacterById(b.getId());
+        a.setSort(bOrigSort);
+        b.setSort(aOrigSort);
+        
+        LessonCharacterTest.compareCharacters(a, a1);
+        LessonCharacterTest.compareCharacters(b, b1);
+    }
+    
+    public void testSwapWords() {
+        LessonWord a = new LessonWord();
+        LessonWord b = new LessonWord();
+        db.addWord(a);
+        db.addWord(b);
+        double aOrigSort = a.getSort();
+        double bOrigSort = b.getSort();
+        assertFalse(aOrigSort == bOrigSort);
+        
+        assertTrue(db.swapWords(a.getId(), aOrigSort,
+                                b.getId(), bOrigSort));
+        
+        LessonWord a1 = db.getWordById(a.getId());
+        LessonWord b1 = db.getWordById(b.getId());
+        a.setSort(bOrigSort);
+        b.setSort(aOrigSort);
+        
+        LessonWordTest.compareWords(a, a1);
+        LessonWordTest.compareWords(b, b1);
+    }
 
 }
