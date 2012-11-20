5e651998cdecde398a09fa1d315d9dd3a3d0e78f
Robert Li <li.robertj+git@gmail.com>
2012-11-19 22:14:40 -0500

    initial commit of empty dbadapter test file

--- tests/src/edu/upenn/cis573/Trace2Win/test/DbAdapterTest.java
+++ tests/src/edu/upenn/cis573/Trace2Win/test/DbAdapterTest.java
@@ -1,21 +1,46 @@
 package edu.upenn.cis573.Trace2Win.test;
 
-import junit.framework.TestCase;
-import edu.upenn.cis573.Trace2Win.*;
+import android.test.AndroidTestCase;
+import android.util.Log;
 import edu.upenn.cis573.Trace2Win.Database.DbAdapter;
-import android.database.sqlite.SQLiteOpenHelper;
+import edu.upenn.cis573.Trace2Win.Database.LessonCharacter;
 
-public class DbAdapterTest extends TestCase {
+public class DbAdapterTest extends AndroidTestCase {
 
-	DbAdapter a;
-	protected void setUp() throws Exception 
-	{
-		a = new DbAdapter(CharacterCreationActivity.class.newInstance());
-		
-	}
-	
-	public void test()
-	{
-		
-	}
+    DbAdapter db;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        dumpDBs();
+        db = new DbAdapter(this.getContext());
+        db.open();
+    }
+
+    protected void tearDown()
+    {
+        dumpDBs();
+    }
+
+    protected void dumpDBs()
+    {
+        for(String str : this.getContext().databaseList())
+        {
+            Log.i("DELETE", str);
+            this.getContext().deleteDatabase(str);
+        }
+    }
+
+    protected void compareCharacters(LessonCharacter expected, LessonCharacter actual)
+    {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getNumStrokes(), actual.getNumStrokes());
+        assertEquals(expected.getItemType(), actual.getItemType());
+        assertEquals(expected.getTags(), actual.getTags());
+        assertEquals(expected.getKeyValues(), actual.getKeyValues());
+    }
+
+
+    // TESTS
+
+
 }
