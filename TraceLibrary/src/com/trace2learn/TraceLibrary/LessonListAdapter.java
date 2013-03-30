be182977096cdecf5cfb7e3ee1ea71221183ae99
Robert Li <li.robertj+git@gmail.com>
2013-03-29 20:59:36 -0400

    Set separate onClick handlers for main item and info button

--- TraceLibrary/src/com/trace2learn/TraceLibrary/LessonListAdapter.java
+++ TraceLibrary/src/com/trace2learn/TraceLibrary/LessonListAdapter.java
@@ -5,8 +5,11 @@ import java.util.List;
 import java.util.Set;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -17,15 +20,17 @@ import com.trace2learn.TraceLibrary.Database.LessonCategory;
 
 public class LessonListAdapter extends ArrayAdapter<Lesson> {
 
-    private ArrayList<Lesson> _items;
+    private ArrayList<Lesson> items;
 
-    private LayoutInflater _vi;
+    private Handler handler;
+    private LayoutInflater vi;
 
-    public LessonListAdapter( Context context, List<Lesson> objects, LayoutInflater vi) 
-    {
+    public LessonListAdapter(Context context, List<Lesson> objects,
+            LayoutInflater vi, Handler handler) {
         super(context, 0, objects);
-        _items = new ArrayList<Lesson>(objects);
-        _vi = vi;
+        this.items = new ArrayList<Lesson>(objects);
+        this.vi = vi;
+        this.handler = handler;
     }
 
     /**
@@ -39,17 +44,18 @@ public class LessonListAdapter extends ArrayAdapter<Lesson> {
         View v = convertView;
 
         if (v == null) {
-            v = _vi.inflate(R.layout.lesson_desc, null);
+            v = vi.inflate(R.layout.lesson_desc, null);
         }
 
-        Lesson      item       = _items.get(position);
-        TextView    nameView   = (TextView)  v.findViewById(R.id.nameView);
-        TextView    sizeView   = (TextView)  v.findViewById(R.id.sizeView);
-        ImageView   category1  = (ImageView) v.findViewById(R.id.category1);
-        ImageView   category2  = (ImageView) v.findViewById(R.id.category2);
-        ImageView   category3  = (ImageView) v.findViewById(R.id.category3);
-        ImageView   category4  = (ImageView) v.findViewById(R.id.category4);
-        ImageView[] categories = {category1, category2, category3, category4};
+        final Lesson item       = items.get(position);
+        TextView     nameView   = (TextView)  v.findViewById(R.id.nameView);
+        TextView     sizeView   = (TextView)  v.findViewById(R.id.sizeView);
+        ImageView    infoButton = (ImageView) v.findViewById(R.id.infoButton);
+        ImageView    category1  = (ImageView) v.findViewById(R.id.category1);
+        ImageView    category2  = (ImageView) v.findViewById(R.id.category2);
+        ImageView    category3  = (ImageView) v.findViewById(R.id.category3);
+        ImageView    category4  = (ImageView) v.findViewById(R.id.category4);
+        ImageView[]  categories = {category1, category2, category3, category4};
 
         int count = item.getNumWords();
         nameView.setText(item.getLessonName());
@@ -69,6 +75,15 @@ public class LessonListAdapter extends ArrayAdapter<Lesson> {
         for (; i < 4; i++) {
             categories[i].setImageResource(0);
         }
+
+        // Set onClick listener for info button
+        infoButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message msg = Message.obtain(handler, 0, item.getStringId());
+                handler.sendMessage(msg);
+            }
+        });
 
         return v;
     }
