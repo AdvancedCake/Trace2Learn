24ec3c88cb953f75c69dc78af1bc1a55cfe00d5b
Robert Li <li.robertj+git@gmail.com>
2013-05-02 18:07:53 -0400

    Changed tests to compile. They probably don't work anymore...

--- Trace2Learn/tests/src/com/trace2learn/Trace2Learn/test/DbAdapterTest.java
+++ Trace2Learn/tests/src/com/trace2learn/Trace2Learn/test/DbAdapterTest.java
@@ -177,14 +177,14 @@ public class DbAdapterTest extends AndroidTestCase {
         lesson.addWord(b.getStringId());
         db.addLesson(lesson);
         
-        List<String> exp1 = Arrays.asList(new String[] {a.getStringId(), b.getStringId()});
-        assertEquals(exp1, db.getWordsFromLessonId(lesson.getStringId()));
+        List<LessonItem> exp1 = Arrays.asList(new LessonItem[] {a, b});
+        assertEquals(exp1, db.getWordsFromLesson(lesson.getStringId()));
         assertEquals(exp1, lesson.getWordIds());
         
         db.swapWordsInLesson(lesson.getStringId(), a.getStringId(), b.getStringId());
         
-        List<String> exp2 = Arrays.asList(new String[] {b.getStringId(), a.getStringId()});
-        assertEquals(exp2, db.getWordsFromLessonId(lesson.getStringId()));
+        List<LessonItem> exp2 = Arrays.asList(new LessonItem[] {b, a});
+        assertEquals(exp2, db.getWordsFromLesson(lesson.getStringId()));
         
         Lesson lesson2 = db.getLessonById(lesson.getStringId());
         assertEquals(exp2, lesson2.getWordIds());
