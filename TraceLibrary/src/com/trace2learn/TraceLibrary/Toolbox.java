da37b662b3a5a679828bb533237a883fc17b21ee
Robert Li <li.robertj+git@gmail.com>
2013-05-26 16:48:12 -0400

    Unused files in src and res directories must be dotfiles.

--- TraceLibrary/src/com/trace2learn/TraceLibrary/Toolbox.java
+++ TraceLibrary/src/com/trace2learn/TraceLibrary/Toolbox.java
@@ -11,7 +11,6 @@ import android.content.res.Resources;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.PopupWindow;
 import android.widget.Toast;
 
 
